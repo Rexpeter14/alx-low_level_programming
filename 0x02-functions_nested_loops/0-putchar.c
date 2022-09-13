31e944d1b535db61a2ed6f25b72b33896c275351
Rexpeter14 <onyemapeter45@gmail.com>
2022-09-13 03:41:03 -0700

    a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "header.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char myvar = "alx";
+
+	while (myvar)
+	{
+		_putchar (myvar);
+		myvarr++;
+	}
+	_putchar ("\n");
+
+	return (0);
+}
