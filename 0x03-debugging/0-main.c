a89553c41731d79cd4355f4e2679851f107a7036
Rexpeter14 <onyemapeter45@gmail.com>
2022-09-15 02:36:03 -0700

    testing for positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
- 
-int main()
+/**
+* fuction that print negative positive value when tested
+*
+* return 0
+*
+*/	
+int main(void)
 {
-    int A;
- 
-    printf("Enter the number A: ");
-    scanf("%d", &A);
- 
-    if (A > 0)
-        printf("%d is positive.", A);
-    else if (A < 0)
-        printf("%d is negative.", A);
-    else if (A == 0)
-        printf("%d is zero.", A);
- 
-    return 0;
+int A;
+
+printf("Enter the number A: ");
+scanf("%d", &A);
+
+if (A > 0)
+printf("%d is positive.", A);
+else if (A < 0)
+printf("%d is negative.", A);
+else if (A == 0)
+printf("%d is zero.", A);
+return (0);
 }
